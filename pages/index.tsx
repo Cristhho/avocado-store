d14d0922bda885d1efaf362c2a5ca643d3cf791e
christian <cristhho@gmail.com>
2020-08-15 14:02:01 -0500

    api endpoints using nextjs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar';
 
 const Home = () => {
+  const [productsList, setProductsList] = useState<TProduct[]>([]);
+
+  useEffect(() => {
+    fetch('/api/avo')
+      .then((res) => res.json())
+      .then(({data, length}) => {
+        setProductsList(data);
+      })
+  }, []);
+
   return (
     <div>
       <Navbar />
       <h1>NextJS</h1>
+      {productsList.map((avo) => {
+        return (
+        <h2>{avo.name}</h2>
+        );
+      })}
     </div>
   );
 };
