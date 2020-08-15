7804740541736d8981381c88e38e0dbccd53fb18
cristhian ochoa <cristhho@gmail.com>
2020-08-15 18:43:59 -0500

    fetching product by id

--- pages/api/avo/[id].ts
+++ pages/api/avo/[id].ts
@@ -3,14 +3,19 @@ import { NextApiRequest, NextApiResponse } from "next";
 import DB from '@database';
 
 const allAvos = async (req: NextApiRequest, res: NextApiResponse) => {
-  const db = new DB();
-  const id = req.query.id;
+  try{
+    const db = new DB();
+    const id = req.query.id;
 
-  const entry = await db.getById(id as string);
-  res.setHeader(
-    'Content-type', 'application/json'
-  );
-  res.status(200).json(entry);
+    const entry = await db.getById(id as string);
+    res.setHeader(
+      'Content-type', 'application/json'
+    );
+    res.status(200).json(entry);
+  } catch(error) {
+    console.error(error);
+    res.status(404).end();
+  }
 }
 
 export default allAvos;
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 const ProductItem = () => {
+  const [product, setProduct] = useState<TProduct | null>(null);
   const router = useRouter();
+
+  useEffect(() => {
+    fetch(`/api/avo/${router.query.id}`)
+      .then((res) => res.json())
+      .then((json: TProduct) => {
+        console.log(json);
+        setProduct(json);
+      });
+  }, [router.query.id]);
+
   return (
     <div>
       Product detail page {router.query.id}
